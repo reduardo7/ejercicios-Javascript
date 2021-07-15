6220c3cc1240f8dfb5e9668006eb58023fa7b334
Eduardo Cuomo <reduardo7@gmail.com>
2021-07-15 11:24:42 -0300

    Ejecrccicios

--- src/moduloEjercicios.js
+++ src/moduloEjercicios.js
@@ -1,36 +1,127 @@
 import basededatos from './basededatos.js';
 
+/**
+ * Clone object.
+ *
+ * @template T
+ * @param {T} o
+ * @returns {T}
+ * @private
+ */
+const _clone = (o) => JSON.parse(JSON.stringify(o));
 
 /**
-* Devuelve el promedio de anios de estreno de todas las peliculas de la base de datos.
-*/
-export const promedioAnioEstreno = () => {
-    return [];
+ * @param {Array<number>} valores
+ * @returns {number}
+ * @private
+ */
+const _promedio = (valores) =>
+  Array.isArray(valores)
+    ? valores.reduce((prev, valor) => valor + prev, 0) / valores.length
+    : 0;
+
+/**
+ * @returns {Array<import('./basededatos').ModelPelicula & {
+ *    promedio: {
+ *      promedio: number,
+ *      total: number,
+ *    },
+ *  }>}
+ * @private
+ */
+const _peliculasPromedios = () => {
+  const resultado = basededatos.calificaciones.reduce(
+    (acc, c) =>
+      acc.set(
+        c.pelicula,
+        (acc.has(c.pelicula) ? acc.get(c.pelicula) : 0) + c.puntuacion
+      ),
+    new Map()
+  );
+
+  return basededatos.peliculas.map((pelicula) => {
+    // Clone object
+    const r = _clone(pelicula);
+
+    r.promedio = {
+      promedio: 0,
+      total: 0,
+    };
+
+    if (resultado.has(pelicula.id)) {
+      const sum = resultado.get(pelicula.id);
+      const total = basededatos.calificaciones.filter(
+        (c) => c.pelicula === pelicula.id
+      ).length;
+
+      r.promedio.promedio = sum / total;
+      r.promedio.total = total;
+    }
+
+    return r;
+  });
 };
 
 /**
-* Devuelve la lista de peliculas con promedio de critica mayor al numero que llega
-* por parametro.
-* @param {number} promedio
-  */
-export const pelicuasConCriticaPromedioMayorA = (promedio) => {
-    return [];
+ * @template T
+ * @param {T} dbTable
+ * @param {keyof T[number]} searchField
+ * @param {string} searchText
+ * @returns {T[number] | null}
+ * @private
+ */
+const _filterBy = (dbTable, searchField, searchText) => {
+  const searchStrClean = searchText.replace(/[^\w\d\-_]+/g, ' ').trim();
+
+  if (searchStrClean) {
+    const searchQ = searchStrClean.toLowerCase().split(/\s+/);
+
+    return dbTable.find((row) => {
+      const fldValue = row[searchField].toLowerCase();
+      return !searchQ.some((q) => !fldValue.includes(q));
+    });
+  }
+
+  return null;
 };
 
 /**
-* Devuelve la lista de peliculas de un director
-* @param {string} nombreDirector
-*/
+ * Devuelve el promedio de anios de estreno de todas las peliculas de la base de datos.
+ */
+export const promedioAnioEstreno = () =>
+  _promedio(basededatos.peliculas.map(({ anio }) => anio));
+
+/**
+ * Devuelve la lista de peliculas con promedio de critica mayor al numero que llega
+ * por parametro.
+ * @param {number} promedio
+ */
+export const pelicuasConCriticaPromedioMayorA = (promedio) =>
+  _peliculasPromedios().filter((pp) => pp.promedio.promedio > promedio);
+
+/**
+ * Devuelve la lista de peliculas de un director
+ * @param {string} nombreDirector
+ */
 export const peliculasDeUnDirector = (nombreDirector) => {
-    return [];
+  const director = _filterBy(basededatos.directores, 'nombre', nombreDirector);
+
+  if (director) {
+    return basededatos.peliculas.filter((p) =>
+      p.directores.includes(director.id)
+    );
+  }
+
+  return [];
 };
 
 /**
-* Devuelve el promdedio de critica segun el id de la pelicula.
-* @param {number} peliculaId
-*/
+ * Devuelve el promdedio de critica segun el id de la pelicula.
+ * @param {number} peliculaId
+ */
 export const promedioDeCriticaBypeliculaId = (peliculaId) => {
-    return [];
+  const pp = _peliculasPromedios().find(({ id }) => id === peliculaId);
+  return pp ? pp.promedio.promedio : 0;
 };
 
 /**
@@ -65,11 +156,10 @@ export const promedioDeCriticaBypeliculaId = (peliculaId) => {
         },
     ],
  */
-export const obtenerPeliculasConPuntuacionExcelente = () => {
-    // Ejemplo de como accedo a datos dentro de la base de datos
-    // console.log(basededatos.peliculas);
-    return [];
-};
+export const obtenerPeliculasConPuntuacionExcelente = (promedioExcelente = 9) =>
+  _peliculasPromedios().filter(
+    (pp) => pp.promedio.promedio >= promedioExcelente
+  );
 
 /**
  * Devuelve informacion ampliada sobre una pelicula.
@@ -119,5 +209,38 @@ export const obtenerPeliculasConPuntuacionExcelente = () => {
  * @param {string} nombrePelicula
  */
 export const expandirInformacionPelicula = (nombrePelicula) => {
-    return {};
+  const peliculaDB = _filterBy(basededatos.peliculas, 'nombre', nombrePelicula);
+
+  if (peliculaDB) {
+    const pelicula = _clone(peliculaDB);
+
+    pelicula.directores = pelicula.directores.map((id) =>
+      _clone(basededatos.directores.find((d) => d.id === id))
+    );
+
+    pelicula.generos = pelicula.generos.map((id) =>
+      _clone(basededatos.generos.find((d) => d.id === id))
+    );
+
+    pelicula.criticas = basededatos.calificaciones
+      .filter((calif) => calif.pelicula === pelicula.id)
+      .map((calif) => {
+        const critico = _clone(
+          basededatos.criticos.find((d) => d.id === calif.critico)
+        );
+
+        critico.pais = basededatos.paises.find(
+          (d) => d.id === critico.pais
+        ).nombre;
+
+        return {
+          critico,
+          puntuacion: calif.puntuacion,
+        };
+      });
+
+    return pelicula;
+  }
+
+  return undefined;
 };
